31b60157988201a11bfb7f16f46076957cbbfc29
xingrufei <xingrufei@yl.com>
2020-03-30 11:08:44 +0800

    添加put方法监控

--- src/main/java/com/skyguard/monitor/agent/AgentClassManager.java
+++ src/main/java/com/skyguard/monitor/agent/AgentClassManager.java
@@ -9,6 +9,7 @@ import net.bytebuddy.matcher.ElementMatchers;
 import net.bytebuddy.utility.JavaModule;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -23,7 +24,7 @@ public class AgentClassManager implements AgentBuilder.Transformer {
     public DynamicType.Builder<?> transform(DynamicType.Builder<?> builder, TypeDescription typeDescription, ClassLoader classLoader, JavaModule javaModule) {
         return builder.visit(Advice
                 .to(AgentInterceptor.class)
-                .on(ElementMatchers.isAnnotatedWith(RequestMapping.class).or(ElementMatchers.isAnnotatedWith(GetMapping.class)).or(ElementMatchers.isAnnotatedWith(PostMapping.class)))
+                .on(ElementMatchers.isAnnotatedWith(RequestMapping.class).or(ElementMatchers.isAnnotatedWith(GetMapping.class)).or(ElementMatchers.isAnnotatedWith(PostMapping.class)).or(ElementMatchers.isAnnotatedWith(PutMapping.class)))
         );
     }
 }
